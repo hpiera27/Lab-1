4541a964412a00201f0af941e91daefa9e6b485a
Herman Piera <hpiera8@gmail.com>
2025-05-08 19:49:49 -0400

    Pushed README.md

---
+++ src/Multiples.java
@@ -0,0 +1,14 @@
+public class Multiples {
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 1; i < 1000; i++) {
+            boolean div3 = (i % 3 == 0);
+            boolean div5 = (i % 5 == 0);
+            if (div3 || div5) {
+                count++;
+            }
+
+        }
+        System.out.println(count);
+    }
+}
